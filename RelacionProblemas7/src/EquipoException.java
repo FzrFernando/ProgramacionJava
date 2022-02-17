0cadaa1b7a930970cf8a9a07406b5d84e2832e03
FzrFernando <fernandozamorarico@gmail.com>
2022-02-17 12:38:20 +0100

    Subiendo ejercicio de equipos de futbol

---
+++ RelacionProblemas7/src/EquipoException.java
@@ -0,0 +1,14 @@
+
+public class EquipoException extends Exception{
+
+	public EquipoException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public EquipoException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ RelacionProblemas7/src/PartidoException.java
@@ -0,0 +1,14 @@
+
+public class PartidoException extends Exception{
+
+	public PartidoException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public PartidoException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
