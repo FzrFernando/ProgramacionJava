4516530db4790634bd859fcd877f162c2c607a7e
FzrFernando <fernandozamorarico@gmail.com>
2022-02-18 09:54:54 +0100

    Modificacion de Pelicula con un Array

--- Peliculas/src/com/jacaranda/Pelicula.java
+++ Peliculas/src/com/jacaranda/Pelicula.java
@@ -4,41 +4,13 @@ public class Pelicula {
 
 	private String nombre;
 	private int estreno;
-	private Personaje personaje1;
-	private Personaje personaje2;
-	private Personaje personaje3;
+	private Personaje personajes[] = new Personaje[3];
 	
-	public Pelicula(String nombre, int estreno, Personaje personaje1, Personaje personaje2, Personaje personaje3) {
+	public Pelicula(String nombre, int estreno, Personaje personajes[]) {
 		super();
 		this.nombre = nombre;
 		this.estreno = estreno;
-		this.personaje1 = personaje1;
-		this.personaje2 = personaje2;
-		this.personaje3 = personaje3;
-	}
-
-	public Personaje getPersonaje1() {
-		return personaje1;
-	}
-
-	public void setPersonaje1(Personaje personaje1) {
-		this.personaje1 = personaje1;
-	}
-
-	public Personaje getPersonaje2() {
-		return personaje2;
-	}
-
-	public void setPersonaje2(Personaje personaje2) {
-		this.personaje2 = personaje2;
-	}
-
-	public Personaje getPersonaje3() {
-		return personaje3;
-	}
-
-	public void setPersonaje3(Personaje personaje3) {
-		this.personaje3 = personaje3;
+		this.personajes = personajes;
 	}
 
 	public String getNombre() {
