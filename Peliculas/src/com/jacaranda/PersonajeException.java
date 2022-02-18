b35eb0aea373c387a108b378c42adfd0a64bc694
FzrFernando <fernandozamorarico@gmail.com>
2022-02-18 14:22:29 +0100

    Javadoc en Exception

--- Peliculas/src/com/jacaranda/PersonajeException.java
+++ Peliculas/src/com/jacaranda/PersonajeException.java
@@ -2,11 +2,18 @@ package com.jacaranda;
 
 public class PersonajeException extends Exception {
 
+	/**
+	 * PersonajeExpection()
+	 */
 	public PersonajeException() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * PersonajeException(String message)
+	 * @param message
+	 */
 	public PersonajeException(String message) {
 		super(message);
 		// TODO Auto-generated constructor stub
