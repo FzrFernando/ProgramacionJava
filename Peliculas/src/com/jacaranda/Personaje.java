a7e41d79b8ade7c848526f7ebf57ff00856133e5
FzrFernando <fernandozamorarico@gmail.com>
2022-02-18 09:09:27 +0100

    Creacion Proyecto Pelicula

---
+++ Peliculas/src/com/jacaranda/Personaje.java
@@ -0,0 +1,84 @@
+package com.jacaranda;
+
+import java.util.Objects;
+
+public class Personaje {
+	
+	private String nombre;
+	private int poder;
+	private Pelicula primeraPelicula;
+	private double estatura;
+	private double peso;
+	private String especie;
+	
+	public Personaje(String nombre, double estatura, double peso, String especie) {
+		super();
+		this.nombre = nombre;
+		this.estatura = estatura;
+		this.peso = peso;
+		this.especie = especie;
+	}
+
+	public Personaje(String nombre, int poder, Pelicula primeraPelicula, double estatura, double peso, String especie) {
+		super();
+		this.nombre = nombre;
+		this.poder = poder;
+		this.primeraPelicula = primeraPelicula;
+		this.estatura = estatura;
+		this.peso = peso;
+		this.especie = especie;
+	}
+
+	public Pelicula getPrimeraPelicula() {
+		return primeraPelicula;
+	}
+
+	public void setPrimeraPelicula(Pelicula primeraPelicula) {
+		this.primeraPelicula = primeraPelicula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public int getPoder() {
+		return poder;
+	}
+
+	public double getEstatura() {
+		return estatura;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public String getEspecie() {
+		return especie;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nombre);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Personaje other = (Personaje) obj;
+		return Objects.equals(nombre, other.nombre);
+	}
+
+	@Override
+	public String toString() {
+		return "Personaje [nombre=" + nombre + ", poder=" + poder + ", primeraPelicula=" + primeraPelicula
+				+ ", estatura=" + estatura + ", peso=" + peso + ", especie=" + especie + "]";
+	}
+	
+	
+}
