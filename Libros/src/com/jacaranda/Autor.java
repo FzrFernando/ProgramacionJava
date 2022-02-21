a64dff960c613bdc073f040f3fd4514087009004
FzrFernando <fernandozamorarico@gmail.com>
2022-02-21 13:28:12 +0100

    Proyecto Libros

---
+++ Libros/src/com/jacaranda/Autor.java
@@ -0,0 +1,9 @@
+package com.jacaranda;
+
+public class Autor {
+	
+	private String nombre;
+	private String apellidos;
+	private String seudonimo;
+	private String nacionalidad;
+}
---
+++ Libros/src/com/jacaranda/Libro.java
@@ -0,0 +1,16 @@
+package com.jacaranda;
+
+import java.time.LocalDate;
+
+public class Libro {
+	
+	private String titulo;
+	private String editorial;
+	private Autor autor;
+	private double precio;
+	private static int iva;
+	private LocalDate fecha_publicacion;
+	private String isbn;
+	
+	
+}
