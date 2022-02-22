e23edd52cb28acf113430ab3cb6563587f66f9df
FzrFernando <fernandozamorarico@gmail.com>
2022-02-22 09:21:13 +0100

    Menú empezado

--- FernandoZamora/src/com/vinos/MainVinos.java
+++ FernandoZamora/src/com/vinos/MainVinos.java
@@ -5,10 +5,13 @@ import java.util.Scanner;
 public class MainVinos {
 
 	public static Scanner teclado = new Scanner (System.in);
+	static final int MAX_PLATO = 3;
+	static int indiceArrayPlato = 0;
+	static Plato [] plato = new Plato[MAX_PLATO];
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 		
 		System.out.println("Bienvenidos al menú del Restaurante Los Pacos");
 		System.out.println("Que opción quiere escoger:" + "\n" +
@@ -21,6 +24,32 @@ public class MainVinos {
 		
 		int opc;
 		opc = Integer.parseInt(teclado.nextLine());
+		
+		while(opc != 6) {
+			switch (opc) {
+			case 1: {
+				break;
+			}
+			case 2: {
+				break;
+			}
+			case 3: {
+				break;
+			}
+			case 4: {
+				
+				break;
+			}
+			case 5: {
+				for (int i = 0; i < plato.length; i++) {
+					plato[i].toString();
+				}
+				break;
+			}
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + opc);
+			}
+		}
 		
 		
 		
