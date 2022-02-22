f18790f04efce951a4e9cde3f518123a543f01d3
FzrFernando <fernandozamorarico@gmail.com>
2022-02-22 08:48:46 +0100

    Clase Plato creada entera

--- FernandoZamora/src/com/vinos/Plato.java
+++ FernandoZamora/src/com/vinos/Plato.java
@@ -1,14 +1,16 @@
 package com.vinos;
 
+import java.util.Objects;
+
 public class Plato {
 
 	/**
 	 * Aquí se mostrarán los atributos de la clase
 	 */
-	private String nombre;
-	private double precio;
-	private static final double IVA = 0.21;
-	private Vino vino;
+	private String nombre; //String con el nombre que tendrá el plato
+	private double precio; //Double con el precio del plato
+	private static final double IVA = 0.21; //Atributo estático y final que será el IVA en double y que podremos modificar a nuestro gusto
+	private Vino vino; //La clase plato recibe un atributo Vino vino
 	
 	public Plato(String nombre) {
 		super();
@@ -40,7 +42,7 @@ public class Plato {
 	
 	public double getPrecioVentaPublico() {
 		double precioConIva;
-		precioConIva = this.precio * this.IVA;
+		precioConIva = this.precio + this.precio * this.IVA;
 		return precioConIva;
 	}
 
@@ -54,6 +56,56 @@ public class Plato {
 
 	public Vino getVino() {
 		return vino;
+	}
+	
+	public void incrementaPrecio(double incremento) {
+		this.precio = getPrecio() + incremento;
+	}
+
+	/**
+	 * El siguiente método será SetVinoRecomendado
+	 * @param vino
+	 */
+	public void setVino(Vino vino) {
+		this.vino = vino;
+	}
+	
+	public void setVinoRecomendado(String vino, double graduacion) {
+		try {
+			Vino v1 = new Vino(vino, graduacion);
+		} catch (VinoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public double getGradosVinosRecomendado() {
+		double gradosRecomendados;
+		gradosRecomendados = this.vino.getGraduacion();
+		return gradosRecomendados;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nombre);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Plato other = (Plato) obj;
+		return Objects.equals(nombre, other.nombre);
+	}
+
+	@Override
+	public String toString() {
+		return "Plato [nombre=" + nombre + ", precio=" + precio + ", getPrecioVentaPublico()=" + getPrecioVentaPublico()
+				+ ", getVino()=" + getVino() + ", getGradosVinosRecomendado()=" + getGradosVinosRecomendado() + "]";
 	}
 	
 	
