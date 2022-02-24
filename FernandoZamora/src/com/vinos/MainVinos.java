ab8d0dbd2f58a0798acda29dfd40a489675064c1
FzrFernando <fernandozamorarico@gmail.com>
2022-02-24 13:46:31 +0100

    Constructor Baraja creado

--- FernandoZamora/src/com/vinos/MainVinos.java
+++ FernandoZamora/src/com/vinos/MainVinos.java
@@ -9,7 +9,7 @@ public class MainVinos {
 	static int indiceArrayPlato = 0;
 	static Plato [] plato = new Plato[MAX_PLATO];
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws PlatoException, VinoException {
 		// TODO Auto-generated method stub
 		
 		
@@ -26,39 +26,71 @@ public class MainVinos {
 		opc = Integer.parseInt(teclado.nextLine());
 		
 		while(opc != 6) {
-			switch (opc) {
-			case 1: {
-				break;
-			}
-			case 2: {
-				break;
-			}
-			case 3: {
-				break;
-			}
-			case 4: {
-				
-				break;
-			}
-			case 5: {
-				for (int i = 0; i < plato.length; i++) {
-					plato[i].toString();
+				switch (opc) {
+				case 1: {
+					boolean encontrado = false;
+					System.out.println("Nombre del plato para dar de alta");
+					String nombre;
+					nombre = teclado.nextLine();
+					System.out.println("Precio del plato para dar de alta");
+					double precio;
+					precio = Double.parseDouble(teclado.nextLine());
+					for (int i = 0; i < plato.length && encontrado==false; i++) {
+						if (plato[i].getNombre().equals(nombre)) {
+							encontrado = true;
+						}
+					}
+					if (encontrado == false) {
+						plato[indiceArrayPlato] =  new Plato(nombre, precio);
+						indiceArrayPlato++;
+					}
+					break;
 				}
-				break;
+				case 2: {
+					boolean encontrado = false;
+					System.out.println("Nombre del plato para cambiarle el precio");
+					String nombre;
+					nombre = teclado.nextLine();
+					Plato p1 = new Plato(nombre);
+					for (int i = 0; i < plato.length; i++) {
+						if (plato[i].getNombre().equals(nombre)) {
+							encontrado = true;
+						}
+					}
+					if (encontrado == true) {
+						double precioModificado;
+						System.out.println("Dime cuanto quieres incrementar el precio");
+						precioModificado = Double.parseDouble(teclado.nextLine());
+						p1.incrementaPrecio(precioModificado);
+					}
+					break;
+				}
+				case 3: {
+					break;
+				}
+				case 4: {
+					String platoPedido;
+					System.out.println("De que plato quiere informacion");
+					platoPedido = teclado.nextLine();
+					for (int i = 0; i < plato.length; i++) {
+						try {
+							if (plato[i].getNombre().equals(platoPedido)) {
+								plato[i].toString();
+							}
+						} catch (Exception e) {
+							e.getMessage();
+						}
+					}
+					break;
+				}
+				case 5: {
+					for (int i = 0; i < plato.length; i++) {
+						plato[i].toString();
+					}
+					break;
+				}
 			}
-			default:
-				throw new IllegalArgumentException("Unexpected value: " + opc);
-			}
-		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		}	
 		
 	}
 
--- Jugar7yMedia/src/com/sieteymedia/Baraja.java
+++ Jugar7yMedia/src/com/sieteymedia/Baraja.java
@@ -2,12 +2,16 @@ package com.sieteymedia;
 
 public class Baraja {
 
-	private int numCartas;
+	private static final int NUMCARTAS=40;
 	private int siguiente;
+	private Carta []cartas;
 	
 	public Baraja() {
 		super();
+		this.cartas = new Carta[NUMCARTAS];
 	}
 	
-	
+	public void barajar() {
+		
+	}
 }
