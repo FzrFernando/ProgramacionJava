796119457eaff42e2c5a34e78707a02fe29680b4
FzrFernando <fernandozamorarico@gmail.com>
2022-03-07 13:26:25 +0100

    Clase Nota por finalizar

---
+++ Peliculas/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.jacaranda","l":"Pelicula"},{"p":"com.jacaranda","l":"Personaje"},{"p":"com.jacaranda","l":"PersonajeException"}];updateSearchResults();
