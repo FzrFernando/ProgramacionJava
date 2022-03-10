6b7ceafbf17e6a245281ca9e82d958629b175dfc
FzrFernando <fernandozamorarico@gmail.com>
2022-03-10 12:56:45 +0100

    Atributos NotaAlarma

--- BlocNotas/src/com/jacaranda/blocnotas/NotaAlarma.java
+++ BlocNotas/src/com/jacaranda/blocnotas/NotaAlarma.java
@@ -1,5 +1,11 @@
 package com.jacaranda.blocnotas;
 
+import java.time.LocalDateTime;
+
 public class NotaAlarma {
 
+	private LocalDateTime fechaAlarma;
+	private static final int MINUTOSREPETIRPORDEFECTO = 5;
+	private int minutosRepetir;
+	private boolean activado;
 }
