144fe6cdb062fada415e184694ee9574a0c60060
FzrFernando <fernandozamorarico@gmail.com>
2022-03-11 09:50:23 +0100

    Constructor bloc

--- BlocNotas/src/com/jacaranda/blocnotas/Bloc.java
+++ BlocNotas/src/com/jacaranda/blocnotas/Bloc.java
@@ -5,4 +5,12 @@ public class Bloc {
 	private static final int NUMERONOTASMAXIMA=3;
 	private int numNotas;
 	private String nombre;
+	
+	public Bloc(String nombre) throws BlocException{
+		super();
+		this.nombre = nombre;
+		this.numNotas = NUMERONOTASMAXIMA;
+	}
+	
+	
 }
