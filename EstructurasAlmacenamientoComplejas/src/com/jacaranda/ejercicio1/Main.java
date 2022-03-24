15dc401f892ce8b5e704e2d7e80735435d0b967f
FzrFernando <fernandozamorarico@gmail.com>
2022-03-24 13:45:52 +0100

    Main creado

--- EstructurasAlmacenamientoComplejas/src/com/jacaranda/ejercicio1/Main.java
+++ EstructurasAlmacenamientoComplejas/src/com/jacaranda/ejercicio1/Main.java
@@ -5,7 +5,16 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Alumno a = new Alumno("Fer","1651");
 		
+		Equipo e = new Equipo("ZariEquipo");
+		
+		try {
+			e.addAlumno(a);
+			System.out.println("Alumno Añadido");
+		} catch (EquipoException e1) {
+			System.out.println(e1.getMessage());
+		}
 	}
 
 }
