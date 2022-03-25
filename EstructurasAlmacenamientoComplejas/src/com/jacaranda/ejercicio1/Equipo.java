e566f9ecd5a3a0c443ceab8c81fce8b5ba189d32
FzrFernando <fernandozamorarico@gmail.com>
2022-03-25 08:29:07 +0100

    Método delALumno

--- EstructurasAlmacenamientoComplejas/src/com/jacaranda/ejercicio1/Equipo.java
+++ EstructurasAlmacenamientoComplejas/src/com/jacaranda/ejercicio1/Equipo.java
@@ -1,26 +1,27 @@
 package com.jacaranda.ejercicio1;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class Equipo {
 
 	private String nombre;
 	private Set<Alumno> listado;
-	
+
 	public Equipo(String nombre) {
 		super();
 		this.nombre = nombre;
 		this.listado = new HashSet<Alumno>();
 	}
-	
+
 	public void addAlumno(Alumno alumno) throws EquipoException {
 		if (listado.contains(alumno) || alumno == null) {
 			throw new EquipoException("Alumno ya existente o nulo");
 		}
 		listado.add(alumno);
 	}
-	
+
 	public void addAlumno2(Alumno alumno) throws EquipoException {
 		if (!listado.add(alumno) || alumno == null) {
 			throw new EquipoException("Alumno ya existente o nulo");
@@ -32,7 +33,7 @@ public class Equipo {
 			throw new EquipoException("No se ha encontrado el alumno o nulo");
 		}
 	}
-	
+
 	public Alumno contenerAlumno(Alumno alumno) throws EquipoException {
 		Alumno aux = null;
 		if (alumno == null) {
@@ -43,13 +44,33 @@ public class Equipo {
 		}
 		return aux;
 	}
-	
+
+	public void delAlumno(String nombre) {
+		Iterator<Alumno> siguiente = this.listado.iterator();
+		while (siguiente.hasNext()) {
+			Alumno a = siguiente.next();
+			if (a.getNombre().equalsIgnoreCase(nombre)) {
+				this.listado.remove(a);
+			}
+		}
+
+	}
+
 	@Override
 	public String toString() {
 		return "Equipo [nombre=" + nombre + ", listado=" + listado + "]";
 	}
-	
+
 	public int numAlumnos() {
 		return this.listado.size();
 	}
+
+//	public Equipo unionEquipo(Equipo e2) {
+//		Equipo resultado = new Equipo("");
+//		for (Alumno a: this.listado) {
+//			if (e2.listado.contains(a)) {
+//				
+//			}
+//		}
+//	}
 }
